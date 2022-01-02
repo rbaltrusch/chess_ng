b2411309f1f372e288ef2d4b2f0bbad23524e626
Richard Baltrusch <richard@baltrusch.net>
2022-01-02 20:44:07 +0100

    Add docstrings

--- chess_engine/piece.py
+++ chess_engine/piece.py
@@ -5,12 +5,16 @@ Created on Mon Feb  8 13:33:57 2021
 @author: Korean_Crimson
 """
 
+from typing import Tuple, List
+
 from chess_engine import move
 from chess_engine.util import convert, convert_str
 from chess_engine.consts import PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
 from chess_engine.move import InitialPawnMove
 
 class Piece:
+    """Piece class. Needs to be subclassed by the various chess pieces"""
+
     def __init__(self, moves, position, representation):
         self.moves = moves
         self.position = convert_str(position) if isinstance(position, str) else position
@@ -22,49 +26,64 @@ class Piece:
     def __repr__(self):
         return f'{self.__class__.__name__}({convert(self.position)})'
 
-    def compute_valid_moves(self, board):
+    def compute_valid_moves(self, board) -> List[Tuple[int, int]]:
+        """Returns a list of squares that the piece can move to or capture at"""
         valid_squares = []
         for move_ in self.moves:
             valid_squares_ = move_.compute_valid_moves(self.position, board, self.team)
             valid_squares.extend(valid_squares_)
         return list(set(valid_squares))
 
-    def move_to(self, position):
+    def move_to(self, position) -> None:
+        """Moves the piece to the specified position and adds it to the position history"""
         print(f'{self.team}: {self.representation} from {convert(self.position)} to {convert(position)}')
         self.position = position
         self.position_history.append(position)
 
 class Pawn(Piece):
+    """Pawn class. Contains all the pawn moves"""
+
     def __init__(self, direction, position, representation):
         moves = [move.InitialPawnMove(direction), move.PawnMove(direction), move.PawnCapture(direction)]
         super().__init__(moves, position, representation)
 
     def move_to(self, position):
+        """Overrides Piece.move_to. Disables InitialPawnMove if the pawn has moved before"""
         super().move_to(position)
         if len(self.position_history) == 1:
             self.moves = [move_ for move_ in self.moves if not isinstance(move_, InitialPawnMove)]
 
 class Knight(Piece):
+    """Knight class. Contains all the knight moves"""
+
     def __init__(self, direction, position, representation):
         moves = []
         super().__init__(moves, position, representation)
 
 class Bishop(Piece):
+    """Bishop class. Contains all the bishop moves"""
+
     def __init__(self, direction, position, representation):
         moves = [move.BishopMove(8)]
         super().__init__(moves, position, representation)
 
 class Rook(Piece):
+    """Rook class. Contains all the rook moves"""
+
     def __init__(self, direction, position, representation):
         moves = [move.RookMove()]
         super().__init__(moves, position, representation)
 
 class Queen(Piece):
+    """Queen class. Contains all the queen moves"""
+
     def __init__(self, direction, position, representation):
         moves = [move.RookMove(), move.BishopMove(8)]
         super().__init__(moves, position, representation)
 
 class King(Queen):
+    """King class. Contains all the king moves"""
+
     def __init__(self, direction, position, representation):
         super().__init__(direction, position, representation)
         for move_ in self.moves:
--- chess_engine/util.py
+++ chess_engine/util.py
@@ -5,13 +5,21 @@ Created on Mon Feb  8 20:35:48 2021
 @author: Korean_Crimson
 """
 
+from typing import Tuple
+
 X_POSITIONS = {k: v for k, v in zip(range(8), 'abcdefgh')}
 
-def convert(position):
+def convert(position: Tuple[int, int]) -> str:
+    """Converts an int index position on the board into a standard chess square name.
+    Example: (0, 7) -> a1
+    """
     x, y = position
     return X_POSITIONS[x] + str(8 - y)
 
-def convert_str(string):
+def convert_str(string: str) -> Tuple[int, int]:
+    """Converts a standard chess square name into an int index position on the board.
+    Example: a1 -> (0, 7).
+    """
     y = 8 - int(string[-1])
     for k, v in X_POSITIONS.items():
         if v == string[0]:
