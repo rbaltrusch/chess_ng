2d413013ae1c658f332bf81c5a0113f0018d8960
Richard Baltrusch <richard@baltrusch.net>
2022-01-07 18:52:57 +0100

    Shift minimax to algorithm.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Created on Mon Feb  8 13:36:04 2021
 import math
 from typing import Tuple
 
-from chess_engine.algorithm import ReversibleMove
+from chess_engine.algorithm import minimax
 from chess_engine.board import Board
 from chess_engine.consts import BLACK
 from chess_engine.consts import BOARD
@@ -29,58 +29,6 @@ def init_pieces() -> Tuple[Team, Team]:
                 new_piece = piece_class(direction, position=(x, y), representation=square)
                 pieces[team].append(new_piece)
     return Team(pieces[WHITE]), Team(pieces[BLACK])
-
-def evaluate(board, team, enemy):
-    """Evaluates the board state based on the amount of moves allied pieces
-    and enemy pieces can make.
-    """
-    #HACK: using compute_all_moves instead of compute_valid_moves to save computation time
-    return len(team.compute_all_moves(board)) - len(enemy.compute_all_moves(board))
-
-#pylint: disable=too-many-arguments,too-many-locals #for now...
-def minimax(board, team, enemy, depth, alpha, beta, maximizing_player):
-    """Minimax algorithm with alpha-beta pruning.
-    At depth=3, computation speed is still relatively fast.
-    At depth=4, it slows down considerably, but does make much better moves.
-    """
-    if depth == 0: #or game over
-        return evaluate(board, team, enemy), None
-
-    if maximizing_player:
-        best_move = None
-        max_eval = -math.inf
-        for piece, position in team.compute_valid_moves(board, enemy.pieces):
-            with ReversibleMove(board, piece, position, enemy.pieces):
-                eval_position = minimax(board, team, enemy, depth-1, alpha, beta, False)[0]
-
-            if eval_position > max_eval or best_move is None:
-                best_move = (piece, position)
-            max_eval = max(max_eval, eval_position)
-            alpha = max(alpha, eval_position)
-            if beta <= alpha:
-                break
-        return max_eval, best_move
-
-    min_evaluation = math.inf
-    min_move = math.inf
-    best_min_move = None
-    for piece, position in enemy.compute_valid_moves(board, team.pieces):
-        with ReversibleMove(board, piece, position, team.pieces):
-            eval_position = minimax(board, team, enemy, depth-1, alpha, beta, True)[0]
-
-        min_evaluation = min(min_evaluation, eval_position)
-        if min_evaluation < min_move:
-            min_move = min_evaluation
-            best_min_move = (piece, position)
-        if best_min_move is None:
-            best_min_move = (piece, position)
-
-        beta = min(beta, eval_position)
-        if beta <= alpha:
-            break
-
-    return min_evaluation, best_min_move
-
 
 def main():
     """Main function"""
